228a3716fd55282fb2a9b6f424e603b93db2ab11
Utkarsh Padia <utkarshpadia@gmail.com>
2024-02-16 23:56:39 +0530

    chore: add form for adding data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer,primary_key = True, autoincrement=True)
     description = db.Column(db.String(),nullable = False)
 
     def __repr__(self):
@@ -20,7 +20,13 @@ class Todo(db.Model):
 with app.app_context():
     db.create_all()
 
-
+@app.route('/todos/create',methods = ['POST'])
+def create_todo():
+    description = request.form.get('description','')
+    todo = Todo(description = description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 @app.route('/')
 def index():
     return render_template('index.html',data = Todo.query.all())
