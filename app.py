c8ccff2ae08c63cc4a3b72d1e618c3ec458ed3ed
Utkarsh Padia <utkarshpadia@gmail.com>
2024-02-17 22:51:39 +0530

    chore: add fetch request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -22,11 +22,13 @@ with app.app_context():
 
 @app.route('/todos/create',methods = ['POST'])
 def create_todo():
-    description = request.form.get('description','')
+    description = request.get_json()['description']
     todo = Todo(description = description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description':todo.description
+    })
 @app.route('/')
 def index():
     return render_template('index.html',data = Todo.query.all())
