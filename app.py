1800e922af463f4ae4f7c410d495ee952a268e22
Utkarsh Padia <utkarshpadia@gmail.com>
2024-02-19 19:45:50 +0530

    chore: add error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import sys
 
 app = Flask(__name__)
 CORS(app)
@@ -22,16 +23,28 @@ with app.app_context():
 
 @app.route('/todos/create',methods = ['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description = description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description':todo.description
-    })
+
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error = True
+        print (sys.exc_info())
+    finally:
+        db.session.close()
+    if (error):
+        abort(400)
+    if (not error):
+        return jsonify(body)
 @app.route('/')
 def index():
     return render_template('index.html',data = Todo.query.all())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
